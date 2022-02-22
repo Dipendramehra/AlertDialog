fb4c5887a04fc4d546d889e92e12571f7cc9b6bd
Dipendramehra <dipendramehra2002@outlook.com>
2022-02-22 15:44:23 +0530

    Update

--- AlertDialogShow/src/main/java/com/dipendra/alertdialogshow/FFmpegActivity.java
+++ AlertDialogShow/src/main/java/com/dipendra/alertdialogshow/FFmpegActivity.java
@@ -1,4 +1,52 @@
 package com.dipendra.alertdialogshow;
 
+import android.util.Log;
+
+import com.arthenica.mobileffmpeg.ExecuteCallback;
+import com.arthenica.mobileffmpeg.FFmpeg;
+import com.arthenica.mobileffmpeg.FFmpegExecution;
+
 public class FFmpegActivity {
+    public static void mergevideoandaudio(String inputvideo,String inputaudio,String outputvideo)
+    {
+        String[] c = {"-i",inputvideo
+                , "-i",inputaudio,"-c:v", "copy", "-c:a", "aac", "-map", "0:v:0", "-map", "1:a:0", "-shortest",
+                outputvideo   };
+
+        FFmpeg.executeAsync(c, new ExecuteCallback() {
+            @Override
+            public void apply( long executionId, int returnCode ) {
+                Log.d("hello" , "return  " + returnCode);
+                Log.d("hello" , "executionID  " + executionId);
+                Log.d("hello" , "FFMPEG  " +  new FFmpegExecution(executionId,c));
+
+            }
+        });
+    }
+    public void videotoaudio(String inputvideo,String outputaudio) {
+        String[] co = {"-i",inputvideo,"-vn",outputaudio };
+        FFmpeg.executeAsync(co, new ExecuteCallback() {
+            @Override
+            public void apply( long executionId, int returnCode ) {
+                Log.d("hello" , "return  " + returnCode);
+                Log.d("hello" , "executionID  " + executionId);
+                Log.d("hello" , "FFMPEG  " +  new FFmpegExecution(executionId,co));
+
+            }
+        });
+    }
+    public void trimvideo(String inputvideo,String outputvideo)
+    {
+        String[] co ={"-y", "-i", inputvideo,"-ss", "00:00:03" , "-to", "00:00:45" , "-c", "copy",outputvideo};
+        FFmpeg.executeAsync(co, new ExecuteCallback() {
+            @Override
+            public void apply( long executionId, int returnCode ) {
+                Log.d("hello" , "return  " + returnCode);
+                Log.d("hello" , "executionID  " + executionId);
+                Log.d("hello" , "FFMPEG  " +  new FFmpegExecution(executionId,co));
+
+            }
+        });
+
+    }
 }
